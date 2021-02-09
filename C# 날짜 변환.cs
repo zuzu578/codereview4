25315aaba80a3d1b44079dc39c084a1be7560202
ZUZU <69393030+zuzu578@users.noreply.github.com>
2021-02-09 10:14:31 +0900

    c# 날짜변환시

---
+++ C# 날짜 변환.cs
@@ -0,0 +1,5 @@
+//2021-02-08 오후 11:09:30  ->  2021-02-08 11:09:30 
+
+ string screenlock_password_approve_time_str = string.Empty;
+ screenlock_password_approve_time_str = (GlobalParam.GetInstance().userInfo.screenlock_password_approve_time.ToString("yyyy-MM-ddTHH:mm:ss"));
+ 
